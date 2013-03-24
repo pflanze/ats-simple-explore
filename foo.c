8e06da82a1105857a5a0f25db170e68fb41183cf
Christian Jaeger <chrjae@gmail.com>
2013-03-24 19:40:14 +0000

    mann wirres C echt wow
    ein echter  what  hacker e  ??

--- foo.c
+++ foo.c
@@ -2,7 +2,8 @@ int main (int argc, char *argv[]) {
   int i, j ;
   for (i = 0; i <= 9; i += 1) {
     for (j = i; j <= 9; j += 1) {
-      if (i < j) printf (", ") ; printf ("(%i, %i)", i, j) ;
+	if (i < j) printf (", ");
+	printf ("(%i, %i)", i, j);
     }
     printf ("\n") ;
   }
